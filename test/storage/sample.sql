651e2b8051500368710383384c9d0f37dfd220df
vasile <elsile69@yahoo.com>
2017-10-26 17:23:41 +0300

    sample.sql updated

--- test/storage/sample.sql
+++ test/storage/sample.sql
@@ -1,8 +1,6 @@
 /*Table structure for table `customers` */
 
-DROP TABLE IF EXISTS `customers`;
-
-CREATE TABLE `customers` (
+CREATE TABLE IF NOT EXISTS  `customers` (
   `customerNumber` int(11) NOT NULL,
   `customerName` varchar(50) NOT NULL,
   `contactLastName` varchar(50) NOT NULL,
@@ -21,9 +19,7 @@ CREATE TABLE `customers` (
 
 /*Data for the table `customers` */
 
-insert  into `customers`(`customerNumber`,`customerName`,`contactLastName`,`contactFirstName`,`phone`,`addressLine1`,`addressLine2`,`city`,`state`,`postalCode`,`country`,`salesRepEmployeeNumber`,`creditLimit`) values 
+insert ignore  into `customers`(`customerNumber`,`customerName`,`contactLastName`,`contactFirstName`,`phone`,`addressLine1`,`addressLine2`,`city`,`state`,`postalCode`,`country`,`salesRepEmployeeNumber`,`creditLimit`) values 
 (103,'Atelier graphique','Schmitt','Carine ','40.32.2555','54, rue Royale',NULL,'Nantes',NULL,'44000','France',1370,'21000.00'),
 (112,'Signal Gift Stores','King','Jean','7025551838','8489 Strong St.',NULL,'Las Vegas','NV','83030','USA',1166,'71800.00'),
 (114,'Australian Collectors, Co.','Ferguson','Peter','03 9520 4555','636 St Kilda Road','Level 3','Melbourne','Victoria','3004','Australia',1611,'117300.00');
-
-
